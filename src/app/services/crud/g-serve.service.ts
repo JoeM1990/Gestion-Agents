81c90d4c1490956d475263bc2d7ca94757f04448
Joe Monkila <jnkiwa25@gmail.com>
2023-11-02 15:31:12 +0100

    init commit

--- src/app/services/crud/g-serve.service.ts
+++ src/app/services/crud/g-serve.service.ts
@@ -12,70 +12,70 @@ export class GServeService {
   constructor( private httpclient: HttpClient) { }
 
   getAllUsers(){
-    return this.httpclient.get<Users[]>('http://localhost:3000/users');
+    return this.httpclient.get<Users[]>('http://localhost:3000/userst');
   }
 
   createUsers(users:Users){
-    return this.httpclient.post('http://localhost:3000/users', users);
+    return this.httpclient.post('http://localhost:3000/userst', users);
   }
 
   getUserById(id:any){
-    return this.httpclient.get('http://localhost:3000/users/'+id);
+    return this.httpclient.get('http://localhost:3000/userst/'+id);
   }
 
 
   updateUser(id:any, users:Users){
-    return this.httpclient.put('http://localhost:3000/users/'+id, users);
+    return this.httpclient.put('http://localhost:3000/userst/'+id, users);
   }
 
   deleteUser(id:any){
-    return this.httpclient.delete('http://localhost:3000/users/'+id);
+    return this.httpclient.delete('http://localhost:3000/userst/'+id);
   }
 
   getAllIndividus(){
-    return this.httpclient.get<Individus[]>('http://localhost:3000/individus');
+    return this.httpclient.get<Individus[]>('http://localhost:3000/individust');
   }
 
   createIndividus(individus:Individus){
-    return this.httpclient.post('http://localhost:3000/individus', individus);
+    return this.httpclient.post('http://localhost:3000/individust', individus);
   }
 
   getIndividusById(id:any){
-    return this.httpclient.get('http://localhost:3000/individus/'+id);
+    return this.httpclient.get('http://localhost:3000/individust/'+id);
   }
 
 
   updateIndividu(id:any, individus:Individus){
-    return this.httpclient.put('http://localhost:3000/individus/'+id, individus);
+    return this.httpclient.put('http://localhost:3000/individust/'+id, individus);
   }
 
   deleteIndividu(id:any){
-    return this.httpclient.delete('http://localhost:3000/individus/'+id);
+    return this.httpclient.delete('http://localhost:3000/individust/'+id);
   }
 
   getAllCasiers(){
-    return this.httpclient.get<Casiers[]>('http://localhost:3000/casiers');
+    return this.httpclient.get<Casiers[]>('http://localhost:3000/casierst');
   }
 
   createCasiers(casiers:Casiers){
-    return this.httpclient.post('http://localhost:3000/casiers', casiers);
+    return this.httpclient.post('http://localhost:3000/casierst', casiers);
   }
 
   getCasiersById(id:any){
-    return this.httpclient.get('http://localhost:3000/casiers/'+id);
+    return this.httpclient.get('http://localhost:3000/casierst/'+id);
   }
 
 
   updateCasiers(id:any, casiers:Casiers){
-    return this.httpclient.put('http://localhost:3000/casiers/'+id, casiers);
+    return this.httpclient.put('http://localhost:3000/casierst/'+id, casiers);
   }
 
   deleteCasiers(id:any){
-    return this.httpclient.delete('http://localhost:3000/casiers/'+id);
+    return this.httpclient.delete('http://localhost:3000/casierst/'+id);
   }
 
   chercheCasier(text:any){
-    return this.httpclient.get('http://localhost:3000/casiers?noms_like='+text);
+    return this.httpclient.get('http://localhost:3000/casierst?noms_like='+text);
   }
   
 }
